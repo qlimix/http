01f8d2cf3968ce25eb5e48b12e2a7c55a7a15325
frank <frank@qlimix.net>
2018-05-16 20:16:12 +0200

    constructor with default values for exceptions

--- src/Exception/NotFoundException.php
+++ src/Exception/NotFoundException.php
@@ -4,4 +4,12 @@ namespace Qlimix\Http\Exception;
 
 final class NotFoundException extends \Exception
 {
+    /**
+     * @param string $message
+     * @param \Throwable $previous
+     */
+    public function __construct(string $message = 'Not found', \Throwable $previous = null)
+    {
+        parent::__construct($message, 0, $previous);
+    }
 }

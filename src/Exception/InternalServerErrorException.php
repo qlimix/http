fce71bf17850cd39b002322f7dd722376b6452ec
frank <frank@qlimix.net>
2018-05-16 20:12:58 +0200

    constructor for internal server error

--- src/Exception/InternalServerErrorException.php
+++ src/Exception/InternalServerErrorException.php
@@ -2,6 +2,16 @@
 
 namespace Qlimix\Http\Exception;
 
+use Throwable;
+
 final class InternalServerErrorException extends \Exception
 {
+    /**
+     * @param Throwable $previous
+     * @param string $message
+     */
+    public function __construct(Throwable $previous, string $message = 'Internal server error')
+    {
+        parent::__construct($message, 0, $previous);
+    }
 }

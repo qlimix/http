2d04dbb086ae36479bb0515da3b02c0f7407e64a
frank <frank@qlimix.net>
2019-02-18 21:52:30 +0100

    from request to request object and can throw exception incase of bad request or response

--- src/Request/Exception/InvalidHttpRequestException.php
+++ src/Request/Exception/InvalidHttpRequestException.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace Qlimix\Http\Validation\Exception;
+namespace Qlimix\Http\Request\Exception;
 
 use Exception;
 use Psr\Http\Message\ResponseInterface;
--- src/Request/FromPsrRequestTransformerInterface.php
+++ src/Request/FromPsrRequestTransformerInterface.php
@@ -3,11 +3,14 @@
 namespace Qlimix\Http\Request;
 
 use Psr\Http\Message\ServerRequestInterface;
+use Qlimix\Http\Request\Exception\InvalidHttpRequestException;
 
 interface FromPsrRequestTransformerInterface
 {
     /**
      * @return static
+     *
+     * @throws InvalidHttpRequestException
      */
     public function createFromRequest(ServerRequestInterface $request);
 }

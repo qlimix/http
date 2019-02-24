59f20b77cc5828d6d209da204e523cabe9f9724f
frank <frank@qlimix.net>
2019-02-24 15:54:25 +0100

    throw an HttpException and return mixed as we don't know any structure that would be returned

--- src/Request/FromPsrRequestTransformerInterface.php
+++ src/Request/FromPsrRequestTransformerInterface.php
@@ -3,14 +3,16 @@
 namespace Qlimix\Http\Request;
 
 use Psr\Http\Message\ServerRequestInterface;
+use Qlimix\Http\Exception\HttpException;
 use Qlimix\Http\Request\Exception\InvalidHttpRequestException;
 
 interface FromPsrRequestTransformerInterface
 {
     /**
-     * @return static
+     * @return mixed
      *
      * @throws InvalidHttpRequestException
+     * @throws HttpException
      */
     public function createFromRequest(ServerRequestInterface $request);
 }
